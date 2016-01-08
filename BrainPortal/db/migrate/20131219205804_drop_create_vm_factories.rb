84de50678b7ebf76965816b66dcc1012acedefde
Tristan Glatard <tristan.glatard@mcgill.ca>
2016-01-08 15:45:43 -0500

    Removed broken DB migrations

--- BrainPortal/db/migrate/20131219205804_drop_create_vm_factories.rb
+++
@@ -1,9 +0,0 @@
-class DropCreateVmFactories < ActiveRecord::Migration
-  def up
-    drop_table :create_vm_factories
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- BrainPortal/db/migrate/20131219210026_drop_create_vm_factories1.rb
+++
@@ -1,9 +0,0 @@
-class DropCreateVmFactories1 < ActiveRecord::Migration
-  def up
-    drop_table :create_vm_factories
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
